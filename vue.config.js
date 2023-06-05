3350364d7c052ba5d801b2d9bc7e4bf9cb46e5d0
juanRoa <juanjaroa@gmail.com>
2023-06-05 17:35:41 -0500

    added module.exports

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: '/portfolio',
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
 }
